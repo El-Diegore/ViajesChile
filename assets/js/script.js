327fbf434a3dfb45b80d576a3c5869581770e559
Diego <pinto.p.diego@gmail.com>
2025-07-09 18:31:16 -0400

    feat: form contact avalaible with jquery validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
-$("#btnEnviar").click(function(){
+$("#forContact").submit(function(e){
+    e.preventDefault()
+    const name = $("#inputName").val().trim();
+    const lastName = $("#inputLastName").val().trim();
+    const email = $("#inputEmail").val().trim();
+    const message = $("#formControlTextarea").val().trim();
+    if(name === "" || lasName === "" || email === "" || message === ""){
+        alert("Debes completar todos los campos");
+    }
     alert("El correo se ha enviado exitosamente!");
+    this.submit();
 })
