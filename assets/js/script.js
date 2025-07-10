1cf2644f2f9307307bfa6150ff2518cf5918ba5e
Diego <pinto.p.diego@gmail.com>
2025-07-10 13:11:32 -0400

    fix: form validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
-$("#forContact").submit(function(e){
+$("#formContact").submit(function(e){
     e.preventDefault()
     const name = $("#inputName").val().trim();
     const lastName = $("#inputLastName").val().trim();
     const email = $("#inputEmail").val().trim();
     const message = $("#formControlTextarea").val().trim();
-    if(name === "" || lasName === "" || email === "" || message === ""){
+    if(name === "" || lastName === "" || email === "" || message === ""){
         alert("Debes completar todos los campos");
     }
     alert("El correo se ha enviado exitosamente!");
     this.submit();
+    this.reset();
 })
