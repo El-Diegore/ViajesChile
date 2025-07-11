7bf49ef941b2b56145f3948f97784cb48bc914d4
Diego <pinto.p.diego@gmail.com>
2025-07-10 20:26:54 -0400

    feat: update form submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+const toastSuccess = new bootstrap.Toast(document.getElementById('toastSuccess'));
 $("#formContact").submit(function(e){
     e.preventDefault()
     const name = $("#inputName").val().trim();
@@ -7,7 +8,30 @@ $("#formContact").submit(function(e){
     if(name === "" || lastName === "" || email === "" || message === ""){
         alert("Debes completar todos los campos");
     }
-    alert("El correo se ha enviado exitosamente!");
-    this.submit();
-    this.reset();
-})
+    const formData = new FormData();
+    formData.append("nombre", name);
+    formData.append("apellido", lastName);
+    formData.append("email", email);
+    formData.append("mensaje", message);
+
+    fetch("https://formspree.io/f/xblyngze",{
+        method: "POST",
+        body: formData,
+        headers: {
+            Accept: "aplication/json"
+        }
+    }).then(Response =>{
+        if(Response.ok){
+            toastSuccess.show()
+            this.reset()
+        }
+        else {
+            alert("Hubo un error, intenta nuevamente");
+        }
+    })
+    .catch(()=>{
+        alert("Hubo un error")
+    });
+
+});
+
